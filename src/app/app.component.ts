956a4788e9e85b9df090c75b9950f80f7bc8ab0b
Greg <39492220+gregorro@users.noreply.github.com>
2024-01-09 21:05:36 +0100

    feat: create form fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'list-of-people';
+  author = 'Grzegorz';
 }
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+
+enum Gender {
+  Male,
+  Female,
+  Other
+}
+
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+  genderEnum: typeof Gender = Gender
+
+  form = new FormGroup({
+    name: new FormControl<string>('', [
+      Validators.required
+    ]),
+    surname: new FormControl<string>('', [
+      Validators.required
+    ]),
+    gender: new FormControl<Gender | null>(null, [
+      Validators.required
+    ]),
+    street: new FormControl<string>('', [
+      Validators.required
+    ]),
+    number: new FormControl<string>('', [
+      Validators.required
+    ]),
+    city: new FormControl<string>('', [
+      Validators.required
+    ]),
+    phoneNumber: new FormControl<string>('', [
+      Validators.required
+    ]),
+    email: new FormControl<string>('', [
+      Validators.required,
+      Validators.email
+    ])
+  })
+
+  onSubmit(){
+    const {errors, valid, value} = this.form
+
+    console.log(`Czy formularz jest zwalidowany? - ${valid ? 'Tak' : 'Nie'}`)
+    console.log(`Errory:`, errors)
+    console.log(`Wartości:`, value)
+  }
+
+}
